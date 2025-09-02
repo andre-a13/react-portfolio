a02be077ec3b4c231a211194387c12bf68d5c673
Arnaud A <arnaud.a.dev@gmail.com>
2025-09-02 17:17:13 +0200

    add character page + routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import i18n from "./config/i18n";
 import { I18nextProvider } from "react-i18next";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import CharacterPage from "./components/pages/Character.tsx";
+import i18n from "./config/i18n";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App/>}/>
+          <Route path="/char" element={<CharacterPage />}/>
+      </Routes>
+      </BrowserRouter>
     </I18nextProvider>
   </StrictMode>
 );
